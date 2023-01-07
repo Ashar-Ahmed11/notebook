42101e09b564e960545edba1a2c2769506d2c5d2
John Doe <johndoe@email.com>
2023-01-08 01:01:18 +0500

    dsada

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 const mongoURI = "mongodb+srv://asharahmed:ILOVEcars123@cluster0.qolymsk.mongodb.net/cloudbook"
-
+mongoose.set('strictQuery', true);
 
 const connectToMongo = () => {
-    mongoose.set('strictQuery', true);
+
     mongoose.connect(mongoURI, () => {
         console.log("CONNECTED TO MONGO SUCCESSFULLY")
     })
