382bfb1c48bda2cfeee74b185a882e16214fe98e
John Doe <johndoe@email.com>
2023-01-08 00:52:13 +0500

    dsad

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.use(cors())
 app.use(express.json())
 
 //ROUTES::
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+// app.use('/api/auth', require('./routes/auth'))
+// app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
